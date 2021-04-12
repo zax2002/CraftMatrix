7f9946500d69ea34ce4e5cc9d006252ff7feaa82
zax2002 <zax@2oo2.ml>
2021-04-12 13:26:18 +0300

    List functionality

--- modules/bot/botBlocks2D.py
+++ modules/bot/botBlocks2D.py
@@ -6,6 +6,9 @@ class BotBlocks2D(Bot, BotBlocks):
 		Bot.__init__(self, config, setCallback)
 		BotBlocks.__init__(self)
 
+		self.listMode = self.config.colorSchemeBlocks.listMode
+		self.list = self.config.colorSchemeBlocks.list
+
 	def _parseCoordinatesAndValue(self, args):
 		if len(args) != 3:
 			return False, None, None
@@ -18,7 +21,14 @@ class BotBlocks2D(Bot, BotBlocks):
 		except ValueError:
 			return False, None, None
 
-		if not self.regexp.match(block):
+		match = self.regexp.findall(block)
+		if len(match) == 0:
 			return False, None, None
+		
+		_, blockId, blockData, _ = match[0]
+
+		if (self.listMode == -1 and blockId not in self.list) or (self.listMode == 1 and blockId in self.list):
+			if blockId not in self.list:
+				return False, None, None
 
 		return True, coordinates, block
